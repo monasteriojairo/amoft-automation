1e1a9e368d68838538a3e3ed1eacafa52c0e147b
monasteriojairo <jairo.m19.p@gmail.com>
2026-02-22 15:31:56 -0600

    Add docstring for serial communication manager
    Added docstring to describe the serial communication manager.

--- src/comm/serial_manager.py
+++ src/comm/serial_manager.py
@@ -1,1 +1,6 @@
 # Handles serial communication to ClearCore and Arduino
+"""
+Serial communication manager
+Handles communication between Raspberry Pi,
+ClearCore controller, and Arduino actuator.
+"""
